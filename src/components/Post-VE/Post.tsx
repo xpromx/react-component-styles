5ab3b0b035a0ccb6e0bb9807cb16e4f65f7480a2
xpromx <xpromx@gmail.com>
2021-12-17 16:29:29 +0900

    feat(vanilla-extract): simple PostCard component v2

--- src/components/Post-VE/Post.tsx
+++ src/components/Post-VE/Post.tsx
@@ -6,7 +6,7 @@ export const Post = () => {
       <img src="https://picsum.photos/600" alt="" className={styles.PostCardImage} />
       <div className={styles.PostCardBody}>
         <h3 className={styles.PostCardTitle}>long established</h3>
-        <p className={styles.PostcardText}>
+        <p className={styles.PostCardText}>
           It is a long established fact that a reader will be distracted by the readable content of
           a page when looking at its layout. The point of using Lorem Ipsum is that....
         </p>
